dc5f4be2082a86f40d06ec27f21c3a6b12726ec6
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-21 23:47:36 +0200

    added aria labels

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,17 @@
-export default function Die({value,isHeld,hold,index}){
-    const coloR = isHeld ? '#59E391' : '#f5f5f5';
-    return (
-        <>
-                <button onClick={() => hold(index)} style={{backgroundColor : coloR}}  className="w-[35.84px] h-[35.84px] font-bold  rounded-[3.81px]  flex items-center justify-center mt-1 shadow-[0px_2px_2px_rgba(0,0,0,0.15)] cursor-pointer">{value}</button>
-        </>
-    );
+export default function Die({ value, isHeld, hold, index }) {
+  const coloR = isHeld ? "#59E391" : "#f5f5f5";
+  return (
+    <>
+      <button
+        aria-pressed={isHeld}
+        aria-label={`Die with value ${value}, 
+            ${isHeld ? "held" : "not held"}`}
+        onClick={() => hold(index)}
+        style={{ backgroundColor: coloR }}
+        className="w-[35.84px] h-[35.84px] font-bold  rounded-[3.81px]  flex items-center justify-center mt-1 shadow-[0px_2px_2px_rgba(0,0,0,0.15)] cursor-pointer"
+      >
+        {value}
+      </button>
+    </>
+  );
 }

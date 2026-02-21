c9c978bcc708becccb294c8d47cf93142531f17d
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-21 23:10:56 +0200

    added the gamewon condition

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,6 +15,9 @@ export default function Main() {
     return newArr;
   };
   const [newDice, setNewDice] = useState(generateAllNewDice());
+  let gameWon =
+    newDice.every((die) => die.isHeld) &&
+    newDice.every((die) => die.value === newDice[0].value);
   const hold = (id) => {
     setNewDice((prevDice) => {
       return prevDice.map((el) =>
@@ -35,6 +38,11 @@ export default function Main() {
     <>
       <main className="w-90 h-94.75 top-17 left-19 bg-[#0B2434] flex  items-center justify-center">
         <div className="w-[320px] h-80 bg-[#f5f5f5]   rounded-[10px] grid  grid-cols-5 gap-y-10 pt-25 place-items-center content-start">
+          {/* <h1 className="">Tenzies</h1>
+          <p className="">
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p> */}
           {newDice.map((element, index) => (
             <Die
               key={element.id}
@@ -48,7 +56,7 @@ export default function Main() {
             onClick={handleRandom}
             className="w-23 h-9 bg-[#5035FF] rounded-md text-white flex justify-center items-center font-bold ml-60 cursor-pointer"
           >
-            Roll Dice
+            {gameWon ? "New Game" : "Roll"}
           </button>
         </div>
       </main>
